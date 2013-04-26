25a0951481c8085a75a7bcebe438f88c961d0970
ruslan <fra.prsnl@i.ua>
2013-04-26 22:23:17 +0300

    Added Reader , ReaderDAO classes and Main class for executing For pom.xml added mysql-connector

---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 26.04.13
+ * Time: 22:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test1 {
+}
