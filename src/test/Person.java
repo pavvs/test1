fee95cf73acf888c393cec8abb1ab0102a3bc837
ruslan <fra.prsnl@i.ua>
2013-04-26 14:02:46 +0300

    Add class Person , delete class Test1, modify POm.xml

---
+++ src/test/Person.java
@@ -0,0 +1,42 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 26.04.13
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
