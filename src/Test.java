3404c7a85c1c1d89d823890140c254dbc1a3e197
pavvs <s_pavel_dp@bk.ru>
2013-04-26 11:20:58 +0300

    First commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pavel
+ * Date: 26.04.13
+ * Time: 11:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+}
